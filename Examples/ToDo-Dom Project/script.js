c9bcab52460a5fcf585edfd32efa811d955aa01f
Ravzanur Cantürk <ravzaur1217@gmail.com>
2021-08-23 16:13:56 +0300

    first commit

--- Examples/ToDo-Dom Project/script.js
+++ Examples/ToDo-Dom Project/script.js
@@ -8,18 +8,21 @@ const taskList = document.querySelector('#task-list');
 //call event listeners
 eventListeners();
 
-function eventListeners(){
+function eventListeners() {
     //submit event
-    form.addEventListener('submit',addNewItem);
+    form.addEventListener('submit', addNewItem);
 
     //delete an item
-    taskList.addEventListener('click',deleteItem);
+    taskList.addEventListener('click', deleteItem);
+
+    //delete all items
+    btnDeleteAll.addEventListener('click', deleteAllItems);
 }
 
 //add new item
-function addNewItem(e){
+function addNewItem(e) {
 
-    if(input.value === ''){
+    if (input.value === '') {
         alert('add new item');
     }
 
@@ -30,20 +33,49 @@ function addNewItem(e){
 
     const a = document.createElement('a');
     a.classList = 'delete-item float-right';
-    a.setAttribute('href','#');
+    a.setAttribute('href', '#');
     a.innerHTML = '<i class="fas fa-times"></i>';
 
     li.appendChild(a);
 
     taskList.appendChild(li);
 
-    input.value=' ';
+    input.value = ' ';
 
     e.preventDefault();
 }
 
 
-function deleteItem(e){
+function deleteItem(e) {
 
-    
+    if (confirm('are you sure??')) {
+        if (e.target.className == 'fas fa-times') {
+            e.target.parentElement.parentElement.remove();
+
+        }
+
+    }
+
+    e.preventDefault();
+
+}
+
+function deleteAllItems(e) {
+
+    if (confirm('are you sure??')) {
+        //taskList.innerHTML = ' ';
+
+        taskList.childNodes.forEach(function (item) {
+            if (item.nodeType === 1) {
+                item.remove();
+            }
+
+        });
+
+    }
+
+
+
+    e.preventDefault();
+
 }
