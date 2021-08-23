5b076401c7b8acfc5c35a9e99ae7e72135d14a5d
Ravzanur Cantürk <ravzaur1217@gmail.com>
2021-08-23 15:19:00 +0300

    first commit

---
+++ Javascript/script.js
@@ -0,0 +1,89 @@
+// console.log("merhaba");
+// console.log(1234);
+// console.log(true);
+// console.log([1,2,3]);
+// console.error("hata olustu");
+// console.warn("bir uyarııı");
+
+
+// console.clear();
+
+
+//Değişkenler
+
+// var age;
+// console.log(age);
+
+// age=20;
+// console.log(age);
+
+// var fname='ravza';
+// console.log(fname);
+
+// var fname='ravzanur';
+// console.log(fname);
+
+// let city='istanbul';
+// console.log(city);
+
+// const email="asd@gmail.com";
+// console.log(email);
+
+
+//Değişken Tipleri
+
+// Primitive Types
+
+// String
+let firstName = 'Çınar';
+console.log(typeof firstName) // string
+
+// Number
+let age = 20; 
+console.log(typeof age)  // number
+
+let money = 100.5;
+console.log(typeof money)  // number
+
+// Boolean
+let isActive = false;
+console.log(isActive) // boolean
+
+// null
+let job = null
+console.log(job) // object
+
+// undefined
+let car;
+console.log(typeof car) // undefined
+
+
+
+// Reference Types: Objects
+
+// Array
+
+let names = ['Ali','Ahmet','Can']
+console.log(names) // object
+
+// Object Literals
+
+let address= {
+   city: 'Kocaeli',
+   country: 'Türkiye'
+}
+
+console.log(address) // object
+
+// Functions
+
+var calculateAge = function(){
+   return 0;
+}
+
+console.log(typeof calculateAge) // function
+
+
+
+
+
