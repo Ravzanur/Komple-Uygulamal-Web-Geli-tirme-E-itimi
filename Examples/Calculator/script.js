f54c225e464d2d5b1b62091a72fb42e4e4b2b5f7
Ravzanur Cantürk <ravzaur1217@gmail.com>
2021-08-26 16:10:04 +0300

    first commit

---
+++ Examples/Calculator/script.js
@@ -0,0 +1,95 @@
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+
+};
+
+function inputDigit(digit){
+    const { displayValue, waitingForSecondOperand } = calculator;
+
+    if(waitingForSecondOperand === true){
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    }else{
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit ;
+    }
+}
+function inputDecimal(dot){
+    if(!calculator.displayValue.includes(dot)){
+        calculator.displayValue += dot;
+    }
+}
+function handleOperator(nextOperator){
+    const { firstOperand, displayValue, operator } = calculator
+
+    const inputValue = parseFloat(displayValue);
+
+    if(operator && calculator.waitingForSecondOperand){
+        calculator.operator = nextOperator;
+        return;
+    }
+    if(firstOperand == null){
+        calculator.firstOperand = inputValue;
+    }else if(operator){
+        const currentValue = firstOperand || 0;
+        const result = performCalculation[operator](currentValue,inputValue);
+
+        calculator.displayValue = String(result);
+        calculator.firstOperand = result;
+    }
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+
+}
+const performCalculation = {
+    '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+    '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+    '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+    '/=': (firstOperand, secondOperand) =>  secondOperand,
+};
+
+function resetCalculator(){
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+}
+
+function showDisplay(){
+    const display = document.querySelector('.screen');
+    display.value = calculator.displayValue;
+    
+}
+
+showDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+    const{ target } = event;
+    if(!target.matches('button')){
+        return;
+    }
+    if (target.classList.contains('operator')){
+        handleOperator(target.value);
+        showDisplay();
+        return;
+    }
+    if (target.classList.contains('decimal')){
+        inputDecimal(target.value);
+        showDisplay();
+        return;
+    }
+    if (target.classList.contains('clear')){
+        resetCalculator();
+        showDisplay();
+        return;
+    }
+
+    inputDigit(target.value);
+    showDisplay();
+
+
+});
